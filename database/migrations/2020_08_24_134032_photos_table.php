6a847b361e42f534ef4f45a886ae52abab75ffe4
jg-digital-media <mail@jonniegrieve.co.uk>
2020-08-24 14:41:33 +0100

    create new migration

--- database/migrations/2020_08_24_134032_photos_table.php
+++ database/migrations/2020_08_24_134032_photos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePhotosTable extends Migration
+class PhotosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,7 @@ class CreatePhotosTable extends Migration
      */
     public function up()
     {
-        Schema::create('photos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        //
     }
 
     /**
@@ -26,6 +23,6 @@ class CreatePhotosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('photos');
+        //
     }
 }
